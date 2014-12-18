f2097a52b35889409d332e93ceb4497c093b59c5
jyzhiyu <jyzhiyu@gmail.com>
2014-12-18 14:15:27 +0800

    1.1
    1.1

---
+++ ESP8266BEE/temp/packagerxl.bat
@@ -0,0 +1,4 @@
+echo off
+netassembler
+concept2cm -forward -export -proj "C:\Users\jyzhiyu\Desktop\IM141118001-Wee-pkg\IM141118001-Wee\ESP8266BEE\esp8266bee.cpm"
+pxl.exe -proj "C:\Users\jyzhiyu\Desktop\IM141118001-Wee-pkg\IM141118001-Wee\ESP8266BEE\esp8266bee.cpm" -nonetassembler
